77183c224f3b3cd58b9d4203220fa4f787415f3d
abdelkareem <81565331+kilofrakh@users.noreply.github.com>
2025-08-11 04:37:32 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -47,4 +47,4 @@ def health_check():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("1:app", host="127.0.0.1", port=8000, reload=True)
