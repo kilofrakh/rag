1406ad6989bb65110921440676bfdbd908c44d73
abdelkareem <81565331+kilofrakh@users.noreply.github.com>
2025-08-20 04:24:30 +0300

    Update pdf_process.py

--- app/services/pdf_process.py
+++ app/services/pdf_process.py
@@ -1,23 +1,19 @@
+from pdfminer.high_level import extract_text
 from typing import List
-from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import io
 
 def split_text(text: str, chunk_size: int = 250, chunk_overlap: int = 27) -> List[str]:
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap
     )
     return splitter.split_text(text)
 
-
 class PDFProcess:
     def extract(self, pdf_file) -> List[str]:
-        reader = PdfReader(pdf_file)
-        full_text = " ".join(page.extract_text() or "" for page in reader.pages)
-        return split_text(full_text)
-
-
+        text = extract_text(pdf_file)
+        return split_text(text or "")
 
     async def upload(self, upload_file) -> io.BytesIO:
         file_bytes = await upload_file.read()
