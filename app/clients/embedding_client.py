dfcfadf598e3b0a0a0e4088368e73e07d9fac14b
abdelkareem <kikoabdelkareem@gmail.com>
2025-08-30 05:28:22 +0300

    .

--- app/clients/embedding_client.py
+++ app/clients/embedding_client.py
@@ -1,9 +1,22 @@
-from sentence_transformers import SentenceTransformer
+import cohere
+import os
+from typing import List
+from app.core.config import config
 
+EMBEDD_KEY = config.EMBEDD_KEY
 
 class EmbeddingClient:
-    def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
-        self.embedder = SentenceTransformer(model_name)
-    
-    def encode(self, text: str):
-        return self.embedder.encode(text).tolist()
+    def __init__(self, api_key: str = EMBEDD_KEY, model_name: str = "embed-english-v3.0"):
+        self.api_key = api_key
+        if not self.api_key:
+            raise ValueError("Cohere API key not found. Please set COHERE_API_KEY env variable.")
+        
+        self.client = cohere.Client(self.api_key)
+        self.model_name = model_name
+
+    def encode(self, text: str) -> List[float]:
+        response = self.client.embed(
+            texts=[text],
+            model=self.model_name
+        )
+        return response.embeddings[0]
--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str 
     ALGORITHM: str 
     ACCESS_TOKEN_EXPIRE_MINUTES: int
-
+    EMBEDD_KEY :str
     model_config= SettingsConfigDict(env_file=".env.prod", env_file_encoding="utf-8")
 
 config = Settings()
